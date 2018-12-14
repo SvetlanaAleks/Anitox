307479bd184a35a09ea6f4575739f5bd9c909b2f
Vladyslav Prodan <imvladprodan@gmail.com>
2018-12-14 11:11:11 +0200

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
 //=require partials/app.js
+//=require partials/youtube.js
+//=require partials/jquery.js
+//=require partials/slick.js
+//=require partials/scripts.js
