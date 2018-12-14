7aa2f415065457732521b3731b393d4db7acaa1f
Svetlana Alekseenko <s.alekseenko@everad.com>
2018-12-14 15:28:16 +0200

    add scroll to target

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -8,8 +8,28 @@ const App = (function () {
             comment.toggleClass('desc--full');
          });
       },
+      scrollToTarget: function (scrollSelector, speed) {
+         const links = $(scrollSelector);
+         links.click(function (e) {
+            e.preventDefault();
+            const _this = $(this);
+            const href = _this.attr('href');
+            if (href.length <= 1) {
+               return;
+            }
+            const target = $(href);
+            if (!target.length) {
+               return;
+            }
+            const top = target.offset().top;
+            $('html, body').animate({
+               scrollTop: top
+            }, speed);
+         });
+      },
       init: function () {
          App.showCommentExpert();
+         App.scrollToTarget('.js-scroll', 700);
       },
    };
 })();
