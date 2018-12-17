c4edc216134484608777f4c5f6ad44330c7f3772
Vladyslav Prodan <imvladprodan@gmail.com>
2018-12-17 10:03:56 +0200

    Fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
 //=require partials/app.js
-<<<<<<< HEAD
-//=require partials/youtube.js
-//=require partials/jquery.js
-//=require partials/slick.js
-//=require partials/scripts.js
-=======
 
-$(document).ready(function () {
-   App.init();
+$(document).ready(function() {
+  App.init();
 });
->>>>>>> cc7504a76e32273af8fd8db7608d4e6a16f0655d
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,47 @@ $(document).ready(function() {
   $(".js_sec-8_slider").slick({
     infinite: true,
     dots: true,
-    arrows: false,
+    arrows: true,
+    prevArrow: $(".slick-prev"),
+    nextArrow: $(".slick-next"),
     slidesToShow: 2,
-    slidesToScroll: 1
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  });
+
+  $(".sec-8-composition-btn").click(function() {
+    $(this)
+      .parent()
+      .toggleClass("active");
+    var text = $(this).text();
+    if (text == "+") {
+      $(this).text("-");
+    } else {
+      $(this).text("+");
+    }
+  });
+});
+
+$(document).ready(function() {
+  $(".plus").click(function() {
+    $(this)
+      .parent()
+      .toggleClass("active");
+    var text = $(this).text();
+    if (text == "+") {
+      $(this).text("-");
+    } else {
+      $(this).text("+");
+    }
   });
 });
